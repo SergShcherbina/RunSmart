bcf27fd75c4847a4192fdee65cab7891c91a4a56
Serg <1990sergik27@gmail.com>
2022-01-25 00:06:29 +0300

    block_catalog+review

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         speed: 1000,
         prevArrow: '<button type="button" class="slick-prev"> <img src="../icons/chevron-left-solid.png"> </button>',
         nextArrow: '<button type="button" class="slick-next"> <img src="../icons/chevron-right-solid.png"> </button>',
-        /* responsive: [
+        responsive: [
             {
               breakpoint: 768,
               settings: {
@@ -15,37 +15,43 @@ $(document).ready(function(){
                 arrows: false
               }
             },
-        ] */
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
+        ] 
+        
     });
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    // $('.catalog-item__link').each(function(i) {
+    //  $(this).on('click', function(e) {
+    //    e.preventDefault();                                     /* убираем стандартное поведение ссылки */
+    //    $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //    $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //  })
+    //}) 
+
+    //$('.catalog-item__back').each(function(i) {
+    //  $(this).on('click', function(e) {                                  
+    //    e.preventDefault();
+    //    $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //    $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //  })
+    //})
+                                /*  снизу более оптимизированный код, суть та же*/
+
+    function toggleSlide(item) {
+      $(item).each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+      })
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
   });
